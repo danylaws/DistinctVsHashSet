8e4d67b01ea0061ea416fd2167e95ead97f8fb61
Lawson <dala@ciep.fr>
2022-04-21 17:07:46 +0200

    Initial project

---
+++ DistinctVsHashSet/Program.cs
@@ -0,0 +1,48 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DistinctVsHashSet
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run(typeof(Program).Assembly);
+
+            //Console.WriteLine(Utility.ListWithDuplicates().Count());
+
+            //Console.WriteLine(Utility.ListWithDuplicates().Distinct().Count());
+
+            //Console.WriteLine(new HashSet<int>(Utility.ListWithDuplicates()).Count());
+
+            Console.ReadKey();
+        }
+
+        [MemoryDiagnoser]
+        public class DistinctVsHashSet
+        {
+            [Benchmark]
+            public List<int> WithDistinct()
+            {
+                var list = Utility.ListWithDuplicates();
+
+                var distinct = list.Distinct().ToList();
+
+                return distinct;
+            }
+
+            [Benchmark]
+            public List<int> WithHashSet()
+            {
+                var list = Utility.ListWithDuplicates();
+
+                var hashset = new HashSet<int>(list).ToList();
+
+                return hashset;
+            }
+        }
+    }
+}
---
+++ DistinctVsHashSet/Utility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DistinctVsHashSet
+{
+    public class Utility
+    {
+        public static List<int> ListWithDuplicates()
+        {
+            var list = new List<int>();
+
+            list.AddRange(Enumerable.Range(0, 10000));
+            list.AddRange(Enumerable.Range(0, 10000));
+            list.AddRange(Enumerable.Range(2000, 10000));
+            list.AddRange(Enumerable.Range(0, 10000));
+            list.AddRange(Enumerable.Range(0, 10000));
+            list.AddRange(Enumerable.Range(0, 1000));
+            list.AddRange(Enumerable.Range(5000, 10000));
+            list.AddRange(Enumerable.Range(500, 1000));
+
+            return list;
+        }
+    }
+}
